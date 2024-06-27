a8ff6779fad39dab907e74129fdcb79f7de46163
ariel07xEd <127271075+ariel07xEd@users.noreply.github.com>
2024-06-27 11:02:04 -0300

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/xEd07.js
@@ -0,0 +1,30 @@
+// Link de la pagina JavasScript: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+
+/* Comentario en bloque:
+    linea 1
+    linea 2
+    linea 3
+*/
+
+let variable = "valor";
+
+const CONSTANTE = "valor";
+
+// Variables de datos primitivos Js
+
+let texto = "string"
+
+let numero = 5;
+
+let esVerdadero = true;
+
+let esFalso = false;
+
+let indefinido;
+
+let nulo = null;
+
+let simbolo = symbol('');
+
+console.log("¡Hola, JavaScript!")
